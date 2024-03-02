39c66bf9f2ef2a5bf877f2edd23e6e4874b35390
seoldonghun <ehdgns5647@gmail.com>
2024-03-02 12:04:40 +0900

    기본 세팅 완료

--- src/main/java/com/example/project1/user/UserController.java
+++ src/main/java/com/example/project1/user/UserController.java
@@ -2,8 +2,13 @@ package com.example.project1.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
