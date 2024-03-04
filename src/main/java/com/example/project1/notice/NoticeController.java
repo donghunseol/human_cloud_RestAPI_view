5e560f282024ce7597ab9e9490616005d0e00a06
minmeanmin <wed1604@naver.com>
2024-03-04 09:23:37 +0900

    notice의 select 구현

--- src/main/java/com/example/project1/notice/NoticeController.java
+++ src/main/java/com/example/project1/notice/NoticeController.java
@@ -1,15 +1,24 @@
 package com.example.project1.notice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class NoticeController {
+
+    private final NoticeRepository noticeRepository;
+
     @GetMapping("/notice" )
-    public String index() {
-        return "notice/main";
+    public String index(HttpServletRequest request) {
+        List<Notice> noticeList = noticeRepository.findAll();
+        request.setAttribute("noticeList", noticeList);
+
+        return "index";
     }
 
     @GetMapping("/notice/saveForm")
--- src/main/java/com/example/project1/notice/NoticeRepository.java
+++ src/main/java/com/example/project1/notice/NoticeRepository.java
@@ -1,23 +1,34 @@
 package com.example.project1.notice;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Component
 @RequiredArgsConstructor
 @Repository
 public class NoticeRepository {
+
     private final EntityManager em;
 
     // 부분 조회
-    public void findById(){
+    public Notice findById(int id){
+        Query query = em.createNativeQuery("select * from notice_tb where id=?", Notice.class);
+        query.setParameter(1, id);
 
+        Notice notice = (Notice) query.getSingleResult();
+        return notice;
     }
 
     // 전체 조회
-    public void findAll(){
-
+    public List<Notice> findAll(){
+        Query query = em.createNativeQuery("select * from notice_tb order by id desc", Notice.class);
+        return query.getResultList();
     }
 
     // 저장
