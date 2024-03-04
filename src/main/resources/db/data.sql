104ab8c461e22bb89eaf65546cf1709f501ddc58
seoldonghun <ehdgns5647@gmail.com>
2024-03-04 17:27:32 +0900

    Scrap 진행중

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -79,11 +79,11 @@ values (3, '2년', 'SQLD', '러브대학교, 3학년', '정보처리데이터과
 
 -- notice
 insert into notice_tb(user_id, title, type, field, work_place, content, deadline, created_at)
-values (1, '공고 제목1', '고용형태1', '개발분야1', '근무지1', '공고 내용1', '2024-03-03',now());
+values (1, '공고 제목1', '고용형태1', '개발분야1', '근무지1', '공고 내용1', '20240303',now());
 insert into notice_tb(user_id, title, type, field, work_place, content, deadline, created_at)
-values (2, '공고 제목2', '고용형태2', '개발분야2', '근무지2', '공고 내용2', '2024-04-04', now());
+values (2, '공고 제목2', '고용형태2', '개발분야2', '근무지2', '공고 내용2', '20240404', now());
 insert into notice_tb(user_id, title, type, field, work_place, content, deadline, created_at)
-values (2, '공고 제목3', '고용형태3', '개발분야3', '근무지3', '공고 내용3', '2024-05-05', now());
+values (2, '공고 제목3', '고용형태3', '개발분야3', '근무지3', '공고 내용3', '20240505', now());
 
 -- scrap
 insert into scrap_tb(user_id, resume_id, notice_id, role, created_at) values(1, 1, null, 1, now());
