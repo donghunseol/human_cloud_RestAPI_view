dbacb1dea471c154d541986069e3ba637e2a57a8
thdus1323 <thdus1323@naver.com>
2024-03-06 11:20:17 +0900

    ì로그인ì ê±구현중

--- src/main/java/com/example/project1/user/UserRepository.java
+++ src/main/java/com/example/project1/user/UserRepository.java
@@ -64,7 +64,7 @@ public class UserRepository {
 
     // 수정
     @Transactional
-    public void update(UserRequest.JoinDTO requsetDTO, int id){
+    public void update(UserRequest.JoinDTO requsetDTO, Integer id){
         Query query = em.createNativeQuery("UPDATE user_tb set address=? where id =?",User.class);
         query.setParameter(1, requsetDTO.getAddress());
         query.setParameter(2,id);
@@ -75,14 +75,17 @@ public class UserRepository {
           Query query = em.createNativeQuery("select * from user_tb where username=? and password=?",User.class);
           query.setParameter(1, requestDTO.getUsername());
           query.setParameter(2, requestDTO.getPassword());
+          User user = (User) query.getSingleResult();
+          return  user;
 
-        User user = null;
-        try {
-            user = (User) query.getSingleResult();
-            return user;
-        } catch (Exception e) {
-           return null;
-        }
+
+//        User user = null;
+//        try {
+//            user = (User) query.getSingleResult();
+//            return user;
+//        } catch (Exception e) {
+//           return null;
+//        }
 
     }
 }
