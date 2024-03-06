e0d414c3a61f11fbe8c842ae81d7dca94170bad4
seoldonghun <ehdgns5647@gmail.com>
2024-03-06 14:51:00 +0900

    저장용

--- src/main/java/com/example/project1/scrap/ScrapController.java
+++ src/main/java/com/example/project1/scrap/ScrapController.java
@@ -1,6 +1,7 @@
 package com.example.project1.scrap;
 
 import com.example.project1.user.User;
+import com.example.project1.user.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -8,32 +9,58 @@ import org.springframework.data.convert.ValueConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class ScrapController {
-    private final HttpSession session;
     private final ScrapRepository scrapRepository;
+    private final UserRepository userRepository;
+    private final HttpSession session;
 
     @GetMapping("/scrap/{id}")
     public String index(@PathVariable Integer id, HttpServletRequest request) {
-        List<ScrapResponse.ScrapDTO> scrap_list = scrapRepository.findById(id);
-        request.setAttribute("scrap_list", scrap_list);
+        List<ScrapResponse.ScrapDTO> scrapList = scrapRepository.findById(id);
+        request.setAttribute("scrapList", scrapList);
+        System.out.println(scrapList);
+//
+//        boolean owner = false; // 페이지 주인 여부 체크
+//        User session_user = (User) request.getAttribute("session_user");
+//
+//        if(session_user != null){
+//            if (1 == session_user.getId()){
+//                owner = true;
+//            }
+//        }
 
-        boolean owner = false; // 페이지 주인 여부 체크
-        User session_user = (User) request.getAttribute("session_user");
+        System.out.println(scrapList);
+        //request.setAttribute("owner", owner);
 
-        if(session_user != null){
-            if (id == session_user.getId()){
-                owner = true;
-            }
+        return "/scrap/main";
+    }
+
+    @PostMapping("/scrap/{id}/save")
+    public String save(@PathVariable Integer id, ScrapRequest.IndividualDTO individualDTO, ScrapRequest.CompanyDTO companyDTO){
+        User user = userRepository.findById(1);
+        System.out.println(user);
+        if (user.getRole() == 0) {
+            scrapRepository.individualSave(individualDTO);
+            System.out.println(individualDTO);
+        }else if(user.getRole() == 1) {
+            scrapRepository.companySave(companyDTO);
+            System.out.println(companyDTO);
         }
 
-        System.out.println(scrap_list);
-        request.setAttribute("owner", owner);
+        return "redirect:/scrap";
+    }
 
-        return "scrap/main";
+
+    @PostMapping("/scrap/{id}/delete")
+    public String delete(@PathVariable(name = "id") Integer id){
+        // System.out.println(id);
+        scrapRepository.deleteById(id);
+        return "redirect:/scrap";
     }
 }
--- src/main/java/com/example/project1/user/UserController.java
+++ src/main/java/com/example/project1/user/UserController.java
@@ -40,14 +40,11 @@ public class UserController {
 
     @PostMapping("/user/login")
     public String login( UserRequest.LoginDTO requestDTO) {
-
         User user = userRepository.findByUsernameAndPassword(requestDTO);
         System.out.println(user);
 
-        session.setAttribute("sessionUser" , user );
+        session.setAttribute("sessionUser" , user);
         System.out.println(user);
-
-
 
         return "redirect:/";
     }
@@ -74,7 +71,7 @@ public class UserController {
     public String updateForm (@PathVariable int id, HttpServletRequest request){
         User sessionUser = (User) session.getAttribute("sessionUser");
         if(sessionUser ==null){
-            return "redirect/loginForm";}
+            return "redirect:/loginForm";}
 
         return "user/updateForm";
     }
