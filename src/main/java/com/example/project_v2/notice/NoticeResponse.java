9aef70898f113493209fec926a967728d6302d96
dongkipark1 <qkrehdrl4417@naver.com>
2024-03-25 17:51:56 +0900

    ãDTO 작성중

--- src/main/java/com/example/project_v2/notice/NoticeResponse.java
+++ src/main/java/com/example/project_v2/notice/NoticeResponse.java
@@ -1,4 +1,23 @@
 package com.example.project_v2.notice;
 
+import lombok.Data;
+
 public class NoticeResponse {
+
+    @Data
+    public static class MainDTO{
+        private Integer noticeId;
+        private String username;
+        private String title;
+        private String deadline;
+        private String image;
+
+        public MainDTO(Notice notice) {
+            this.noticeId = noticeId;
+            this.username = username;
+            this.title = title;
+            this.deadline = deadline;
+            this.image = image;
+        }
+    }
 }
