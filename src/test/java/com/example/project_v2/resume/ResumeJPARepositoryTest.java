85ed805453dc5771c630d07013325bb7c0e77c9e
minmeanmin <wed1604@naver.com>
2024-03-28 12:26:28 +0900

    delete_test, findAll_test 구현

--- src/test/java/com/example/project_v2/resume/ResumeJPARepositoryTest.java
+++ src/test/java/com/example/project_v2/resume/ResumeJPARepositoryTest.java
@@ -1,10 +1,12 @@
 package com.example.project_v2.resume;
 
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -30,5 +32,34 @@ public class ResumeJPARepositoryTest {
         // Then
         assertTrue(optionalResume.isPresent()); // 해당 id에 해당하는 Resume가 존재하는지 확인
         assertNotNull(optionalResume.get().getUser()); // Resume에 연결된 User가 존재하는지 확인
+    }
+
+    @Test
+    public void delete_test(){
+        // given
+        int id = 1;
+
+        // when
+        resumeJPARepository.deleteById(1);
+        em.flush();
+
+        List<Resume> resumeList = resumeJPARepository.findAll();
+
+        // then
+        Assertions.assertThat(resumeList.size()).isEqualTo(5);
+
+    }
+
+    @Test
+    public void findAll_test(){
+        // given
+
+
+        // when
+        List<Resume> resumeList = resumeJPARepository.findAll();
+
+        // then
+        Assertions.assertThat(resumeList.size()).isEqualTo(6);
+
     }
 }
