568c8c50d911a236b4d5c9988aef4cf40ccc8484
dongkipark1 <qkrehdrl4417@naver.com>
2024-03-28 12:04:31 +0900

    notice 목록 보기 완료

--- src/test/java/com/example/project_v2/notice/NoticeJPARepositoryTest.java
+++ src/test/java/com/example/project_v2/notice/NoticeJPARepositoryTest.java
@@ -1,13 +1,16 @@
 package com.example.project_v2.notice;
 
+import com.example.project_v2.board.Board;
 import com.example.project_v2.user.UserJPARepository;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -49,6 +52,21 @@ public class NoticeJPARepositoryTest {
         assertTrue(noticeOP.isPresent());
         assertEquals(id, noticeOP.get().getId());
         assertNotNull(noticeOP.get().getUser()); // User 정보도 함께 로드되었는지 확인
+    }
+
+    @Test
+    public void findAll_test() {
+        //given
+
+        //when
+        List<Notice> noticeList = noticeJPARepository.findAll();
+
+        //then
+        System.out.println("findAll_test/size : " + noticeList.size()); // 공고글 목록 수
+        System.out.println("findAll_test/username : " + noticeList.get(2).getUser().getUsername()); //공고 쓴 유저 확인
+
+        assertThat(noticeList.size()).isEqualTo(5);
+        assertThat(noticeList.get(2).getUser().getUsername()).isEqualTo("comlove");
     }
 
 }
