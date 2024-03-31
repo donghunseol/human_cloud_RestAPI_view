92587b5866370863619fbd4c6b691350604bfe56
seoldonghun <ehdgns5647@gmail.com>
2024-04-01 00:04:13 +0900

    love/save/sdh dto/service/controller

--- src/main/java/com/example/project_v2/love/LoveController.java
+++ src/main/java/com/example/project_v2/love/LoveController.java
@@ -1,24 +1,24 @@
 package com.example.project_v2.love;
 
 import com.example.project_v2._core.util.ApiUtil;
+import com.example.project_v2.user.User;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class LoveController {
 
-    private final LoveJPARepository loveJPARepository;
+    private final LoveService loveService;
     private final HttpSession session;
 
     // 좋아요 입력
     @PostMapping("/api/loves")
-    public ResponseEntity<?> save(){
+    public ResponseEntity<?> save(@RequestBody LoveRequest.SaveDTO reqDTO){
+        User user = (User) session.getAttribute("sessionUser");
+        loveService.save(reqDTO, user);
         return ResponseEntity.ok(new ApiUtil<>(null));
     }
 
--- src/main/java/com/example/project_v2/love/LoveService.java
+++ src/main/java/com/example/project_v2/love/LoveService.java
@@ -1,10 +1,22 @@
 package com.example.project_v2.love;
 
+import com.example.project_v2._core.errors.exception.Exception401;
+import com.example.project_v2._core.errors.exception.Exception403;
+import com.example.project_v2.board.BoardJPARepository;
+import com.example.project_v2.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class LoveService {
     private final LoveJPARepository loveJPARepository;
+    private final BoardJPARepository boardJPARepository;
+
+    @Transactional
+    public Love save(LoveRequest.SaveDTO reqDTO, User sessionUser){
+        Love love = loveJPARepository.save(reqDTO.toEntity(sessionUser));
+        return love;
+    }
 }
