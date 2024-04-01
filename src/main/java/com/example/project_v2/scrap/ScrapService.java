11127b2e87ba843acf1c61fdf17709d49a226dcc
minmeanmin <wed1604@naver.com>
2024-04-01 17:14:15 +0900

    @Transactional 추가

--- src/main/java/com/example/project_v2/scrap/ScrapService.java
+++ src/main/java/com/example/project_v2/scrap/ScrapService.java
@@ -5,6 +5,7 @@ import com.example.project_v2.notice.NoticeJPARepository;
 import com.example.project_v2.resume.Resume;
 import com.example.project_v2.resume.ResumeJPARepository;
 import com.example.project_v2.user.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class ScrapService {
     private final ScrapJPARepository scrapJPARepository;
+
+    @Transactional
     public Scrap save(Resume resume, ScrapRequest.SaveDTO reqDTO, User sessionUser) {
         Scrap scrap = scrapJPARepository.save(reqDTO.toEntity(sessionUser, resume));
         return scrap;
     }
 
+    @Transactional
     public Scrap save(Notice notice, ScrapRequest.SaveDTO reqDTO, User sessionUser) {
         Scrap scrap = scrapJPARepository.save(reqDTO.toEntity(sessionUser, notice));
         return scrap;
