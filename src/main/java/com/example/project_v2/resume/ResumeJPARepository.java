a2de8992d079bc7601aa8cc3db8c2f133130a537
tkffkels93 <tkffkels93@gmail.com>
2024-04-01 19:50:11 +0900

    메인페이지 기술 검색 완료

--- src/main/java/com/example/project_v2/resume/ResumeJPARepository.java
+++ src/main/java/com/example/project_v2/resume/ResumeJPARepository.java
@@ -13,6 +13,9 @@ public interface ResumeJPARepository extends JpaRepository<Resume, Integer> {
     @Query("select r from Resume r join fetch r.user u where r.id = :id")
     Optional<Resume> findByIdJoinUser(@Param("id") int id);
 
+    @Query("select r from Resume r join fetch r.skills s where s.name like %:skillName%")
+    List<Resume> findBySkill(@Param("skillName") String skillName, Sort sort);
+
     @Query("select r from Resume r join fetch r.user u where u = :user")
     List<Resume> findByUser(@Param("user") User user, Sort sort);
 }
