c3f15a0b0ef3c69e9f4751d38d8adb0663830967
dongkipark1 <qkrehdrl4417@naver.com>
2024-04-02 11:01:56 +0900

    합격 불합격 service

--- src/main/java/com/example/project_v2/apply/ApplyService.java
+++ src/main/java/com/example/project_v2/apply/ApplyService.java
@@ -22,6 +22,16 @@ public class ApplyService {
     private final NoticeJPARepository noticeJPARepository;
     private final ResumeJPARepository resumeJPARepository;
 
+    // 합격, 불합격
+    @Transactional
+    public Apply resumePass(ApplyRequest.PassDTO passDTO, User user){
+        Apply apply = applyJPARepository.findById(passDTO.getId())
+                .orElseThrow(() -> new Exception404("지원 번호를 찾을 수 없습니다"));
+        apply.setPass(passDTO.isPass());
+        return applyJPARepository.save(apply);
+    }
+
+
 
     // 이력서 조회
     public ApplyResponse.SelectResumeDTO findById(Integer applyId, User sessionUser){
